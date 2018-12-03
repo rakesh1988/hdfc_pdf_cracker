b02d3e3fc926771e2baf4a97901feb10e4b46c38
rakesh kashyap <rakesh1988@gmail.com>
2018-12-04 01:48:23 +0800

    now can decrypt with date of birth as well.. caution: dirty code

--- hdfc_pdf_crack.py
+++ hdfc_pdf_crack.py
@@ -4,6 +4,7 @@ import subprocess
 import threading 
 import argparse
 from multiprocessing import Process, Pool
+from datetime import datetime, timedelta
 
 parser = argparse.ArgumentParser()  
 
@@ -17,8 +18,8 @@ inputFile = pypdf.PdfFileReader(open(filePath, "rb"))
 output = pypdf.PdfFileWriter()
 
 def decryptFile(customerId):
-	if(customerId%1000==0):
-		print(customerId)
+	# if(customerId%1000==0):
+	# 	print(customerId)
 	try:
 		inputFile.decrypt(str(customerId)) #if this fails goes to except
 		for pageNumber in range (0, inputFile.getNumPages()):
@@ -36,37 +37,44 @@ def looper(start: int,end: int):
 		#print(currCustId)
 		decryptFile(currCustId)
 
-p1 = Process(target=looper, args=(10000000,20000000))
-p2 = Process(target=looper, args=(20000000,30000000))
-p3 = Process(target=looper, args=(30000000,40000000))
-p4 = Process(target=looper, args=(40000000,50000000))
-p5 = Process(target=looper, args=(50000000,60000000))
-p6 = Process(target=looper, args=(60000000,70000000))
-p7 = Process(target=looper, args=(70000000,80000000))
-p8 = Process(target=looper, args=(80000000,90000000))
-p9 = Process(target=looper, args=(90000000,100000000))
+def dateGenerator(year: str):
+	startDate = datetime.strptime("01/01/"+str(year),"%d/%m/%Y")
+	for x in range(366):
+		startDate = startDate + timedelta(days=1)
+		decryptFile(startDate.strftime("%d%m%Y"))	
+
+p1 = dateGenerator(1988)
+#rocess(target=looper, args=(0,9999))
+# p2 = Process(target=looper, args=(20000000,30000000))
+# p3 = Process(target=looper, args=(30000000,40000000))
+# p4 = Process(target=looper, args=(40000000,50000000))
+# p5 = Process(target=looper, args=(50000000,60000000))
+# p6 = Process(target=looper, args=(60000000,70000000))
+# p7 = Process(target=looper, args=(70000000,80000000))
+# p8 = Process(target=looper, args=(80000000,90000000))
+# p9 = Process(target=looper, args=(90000000,100000000))
 
 
 p1.start()
-p2.start()
-p3.start()
-p4.start()
-p5.start()
-p6.start()
-p7.start()
-p8.start()
-p9.start()
+# p2.start()
+# p3.start()
+# p4.start()
+# p5.start()
+# p6.start()
+# p7.start()
+# p8.start()
+# p9.start()
 
 
 p1.join()
-p2.join()
-p3.join()
-p4.join()
-p5.join()
-p6.join()
-p7.join()
-p8.join()
-p9.join()
+# p2.join()
+# p3.join()
+# p4.join()
+# p5.join()
+# p6.join()
+# p7.join()
+# p8.join()
+# p9.join()
 
 
 print("done")
